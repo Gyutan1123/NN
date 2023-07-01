8744df0067853c475df5066130eaf259bc5b8a44
Gyutan1123 <raito1123@ezweb.ne.jp>
2023-07-01 16:53:40 +0900

    不要な関数を削除しました 各関数にコメントをつけました

--- NN.c
+++ NN.c
@@ -1,6 +1,5 @@
 #include "nn.h"
-
-#include "MT.h"
+#include "MT.h" 
 #define pi 3.14159265358979323846264338327950288 /* 円周率*/
 
 /* 動的メモリ確保された配列 n 個 をまとめてfreeする */
@@ -12,7 +11,7 @@ void free_all(int n, ...) {
     }
     va_end(ap);
 }
-
+/* サイズm*nの配列xをm*n行列とみなし、そのように出力する*/
 void print(int m, int n, const float *x) {
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < n; j++) {
@@ -21,7 +20,7 @@ void print(int m, int n, const float *x) {
         printf("\n");
     }
 }
-
+/*m*n行列A, n*1行列 x, m*1行列 b, y について、 y = Ax + b とする*/
 void fc(int m, int n, const float *x, const float *A, const float *b,
         float *y) {
     for (int k = 0; k < m; k++) {
@@ -31,7 +30,7 @@ void fc(int m, int n, const float *x, const float *A, const float *b,
         y[k] += b[k];
     }
 }
-
+/*サイズnの配列xにrelu関数を適用し、その結果をyに記録する*/
 void relu(int n, const float *x, float *y) {
     for (int k = 0; k < n; k++) {
         if (x[k] > 0) {
@@ -41,7 +40,7 @@ void relu(int n, const float *x, float *y) {
         }
     }
 }
-
+/*サイズnの配列xにsoftmax関数を適用し、その結果をyに記録する*/
 void softmax(int n, const float *x, float *y) {
     float xmax = x[0];
     for (int k = 0; k < n; k++) {
@@ -58,23 +57,8 @@ void softmax(int n, const float *x, float *y) {
     }
 }
 
-int inference3(const float *A, const float *b, const float *x) {
-    float *y = malloc(sizeof(float) * 10);
-    for (int i = 0; i < 10; i++) y[i] = 0.0f;
-    fc(10, 784, x, A, b, y);
-    relu(10, y, y);
-    softmax(10, y, y);
-    float ymax = y[0];
-    int ymax_index = 0;
-    for (int i = 0; i < 10; i++) {
-        if (y[i] > ymax) {
-            ymax = y[i];
-            ymax_index = i;
-        }
-    }
-    return ymax_index;
-}
-
+/* n*1行列である、softmax層の出力yおよび、NNの推論による出力の理想値であるtを用いて
+下流への勾配dEdxを計算する */
 void softmaxwithloss_bwd(int n, const float *y, unsigned char t, float *dEdx) {
     for (int i = 0; i < n; i++) {
         dEdx[i] = y[i];
@@ -83,7 +67,7 @@ void softmaxwithloss_bwd(int n, const float *y, unsigned char t, float *dEdx) {
         }
     }
 }
-
+/* n*1行列 x を入力とする relu層において、上流からの勾配dEdy によって下流への勾配dEdxを計算する */
 void relu_bwd(int n, const float *x, const float *dEdy, float *dEdx) {
     for (int i = 0; i < n; i++) {
         if (x[i] > 0) {
@@ -93,7 +77,8 @@ void relu_bwd(int n, const float *x, const float *dEdy, float *dEdx) {
         }
     }
 }
-
+/* m*n行列A, m*1行列b について、fc層に入力されるn*1行列x, m*1行列で表される上流の層からの勾配dEdyを用いてdEdA,dEdbを計算
+また、下流への勾配dEdxを計算する */
 void fc_bwd(int m, int n, const float *x, const float *dEdy, const float *A,
             float *dEdA, float *dEdb, float *dEdx) {
     for (int i = 0; i < m; i++) {
@@ -110,80 +95,52 @@ void fc_bwd(int m, int n, const float *x, const float *dEdy, const float *A,
     }
 }
 
-void backward3(const float *A, const float *b, const float *x, unsigned char t,
-               float *y, float *dEdA, float *dEdb) {
-    for (int i = 0; i < 10; i++) y[i] = 0.0f;
-    float *FC_in = malloc(sizeof(float) * 784);
-    for (int i = 0; i < 784; i++) {
-        FC_in[i] = x[i];
-    }
-    fc(10, 784, x, A, b, y);
-    float *ReLU_in = malloc(sizeof(float) * 10);
-    for (int i = 0; i < 10; i++) {
-        ReLU_in[i] = y[i];
-    }
-    relu(10, y, y);
-    softmax(10, y, y);
-    float *dEdx_soft = malloc(sizeof(float) * 10);
-    float *dEdx_relu = malloc(sizeof(float) * 10);
-    float *dEdx_fc = malloc(sizeof(float) * 784);
-    softmaxwithloss_bwd(10, y, t, dEdx_soft);
-    relu_bwd(10, ReLU_in, dEdx_soft, dEdx_relu);
-    fc_bwd(10, 784, FC_in, dEdx_relu, A, dEdA, dEdb, dEdx_fc);
-
-    free_all(5, dEdx_soft, dEdx_relu, dEdx_fc, ReLU_in, FC_in);
-}
-
+/*損失関数E=-Σt_klog(y_k) を計算し、それを戻り値とするとする。0の対数をとってしまわないように、微小量1e-7を加える*/
 float cross_entropy_error(const float *y, int t) { return -log(y[t] + 1e-7); }
 
+/*サイズnの配列x,oに対し、oの各要素にxの各要素を足す*/
 void add(int n, const float *x, float *o) {
     for (int i = 0; i < n; i++) {
         o[i] += x[i];
     }
 }
 
+/*サイズ size の配列x,o について、oの各要素に、xの各要素の 1/n倍を足す*/
 void add_ave(int size, int n, const float *x, float *o) {
     for (int i = 0; i < size; i++) {
         o[i] += (float)1 / n * x[i];
     }
 }
 
-void scale_and_add(int n, float scale, float *x, float *y) {
-    for (int i = 0; i < n; i++) {
-        y[i] += scale * x[i];
-    }
-}
-
+/* サイズ n の配列 o の各要素を x倍する */
 void scale(int n, float x, float *o) {
     for (int i = 0; i < n; i++) {
         o[i] *= x;
     }
 }
 
-void sqrt_and_div(int n, float *x, float *y, float *o) {
-    for (int i = 0; i < n; i++) {
-        o[i] = y[i] / sqrt(x[i] + 1e-7);
-    }
-}
-
+/* サイズnの配列oを各要素 x で初期化する */
 void init(int n, float x, float *o) {
     for (int i = 0; i < n; i++) {
         o[i] = x;
     }
 }
 
+/* [0:1]の一様乱数従うfloat型の値を返却する */
 float rand01() {
     return (float)genrand_real1();
     /* return rand() / RAND_MAX * 2 - 1; */
 }
 
+/*サイズnの配列oを各要素[0:1]の一様乱数で初期化する*/
 void rand_init(int n, float *o) {
     for (int i = 0; i < n; i++) {
-        float x = (float)rand() / RAND_MAX * 2 - 1;
+        float x = rand01();
         o[i] = x;
     }
 }
 
+/*0~n-1 について、x[i] を x[j] (jは0~n-1の乱数)と入れ替えることで、サイズ n の配列 x をシャッフルする*/
 void shuffle(int n, int *x) {
     for (int i = 0; i < n; i++) {
         int j = rand() % n;
@@ -193,6 +150,9 @@ void shuffle(int n, int *x) {
     }
 }
 
+/* 各fc層のパラメータ、入力 x、出力結果ｙを表す配列を入力として、
+順番に計算していきyを求める。
+その後、yの要素のうち最大のものの index を求め、それを返り値とする。 */
 int inference6(const float *A1, const float *b1, const float *A2,
                const float *b2, const float *A3, const float *b3,
                const float *x, float *y) {
@@ -203,10 +163,8 @@ int inference6(const float *A1, const float *b1, const float *A2,
     float *y2 = malloc(sizeof(float) * 100);
     init(100, 0, y2);
     fc(100, 50, y1, A2, b2, y2);
-
     relu(100, y2, y2);
     fc(10, 100, y2, A3, b3, y);
-
     softmax(10, y, y);
     float ymax = y[0];
     int ymax_index = 0;
@@ -221,12 +179,16 @@ int inference6(const float *A1, const float *b1, const float *A2,
     return ymax_index;
 };
 
+/* サイズ n の配列 x, y について、yの各要素を x の各要素と等しくする */
 void copy(int n, const float *x, float *y) {
     for (int i = 0; i < n; i++) {
         y[i] = x[i];
     }
 }
 
+/* 各fc層のパラメータ、NNに対する入力 x 答え t , 損失関数EのA1 ~ b3 の偏微分を表す配列を入力とし、
+各層への入力をそれぞれ記録しながら、順にNNの構成にしたがって計算していく。
+その後、最終的な計算結果から、逆方向に勾配を計算していき、EのA1~b3の偏微分を計算する*/
 void backward6(const float *A1, const float *b1, const float *A2,
                const float *b2, const float *A3, const float *b3,
                const float *x, unsigned char t, float *dEdA1, float *dEdb1,
@@ -253,7 +215,6 @@ void backward6(const float *A1, const float *b1, const float *A2,
     copy(100, y2, FC3_in);
     fc(10, 100, y2, A3, b3, y3);
     softmax(10, y3, y3);
-
     float *dEdx_soft = malloc(sizeof(float) * 10);
     float *dEdx_fc3 = malloc(sizeof(float) * 100);
     float *dEdx_relu2 = malloc(sizeof(float) * 100);
@@ -266,11 +227,11 @@ void backward6(const float *A1, const float *b1, const float *A2,
     fc_bwd(100, 50, FC2_in, dEdx_relu2, A2, dEdA2, dEdb2, dEdx_fc2);
     relu_bwd(50, ReLU1_in, dEdx_fc2, dEdx_relu1);
     fc_bwd(50, 784, FC1_in, dEdx_relu1, A1, dEdA1, dEdb1, dEdx_fc1);
-
     free_all(13, y1, y2, FC1_in, FC2_in, FC3_in, ReLU1_in, ReLU2_in, dEdx_soft,
              dEdx_fc1, dEdx_fc2, dEdx_fc3, dEdx_relu1, dEdx_relu2);
 }
 
+/* 指定したファイル名に、ポインタA,bが指しているサイズ m*n , mの配列を、m*n , m*1 行列として保存する */
 void save(const char *filename, int m, int n, const float *A, const float *b) {
     FILE *write;
     write = fopen(filename, "wb");
@@ -278,28 +239,29 @@ void save(const char *filename, int m, int n, const float *A, const float *b) {
     fwrite(b, sizeof(float), m, write);
 }
 
+/* 指定したバイナリファイルに保存されている、m*n行列A,m*1行列b を、ポインタA,bが示す配列に格納する
+ファイルが存在しない場合はエラー文を表示する*/
 void load(const char *filename, int m, int n, float *A, float *b) {
     FILE *read;
     read = fopen(filename, "rb");
     if (!read) {
-        printf("Cannot open %s.\n", *filename);
+        printf("Cannot open %s.\n", filename);
     } else {
         fread(A, sizeof(float), m * n, read);
         fread(b, sizeof(float), m, read);
     }
 }
 
+/* Box-Muller法と変数変換により、標準偏差\sqrt{\frac{2}{n}} のガウス分布を実装*/
 void gaussian_rand_init(int n, float *o) {
     for (int i = 0; i < n; i++) {
-        float u1 = (float)genrand_real1();
-        float u2 = (float)genrand_real1();
-        /* float u1 = (float)rand() / RAND_MAX;
-        float u2 = (float)rand() / RAND_MAX; */
-        o[i] =
-            sqrt((float)2 / n) * (float)sqrt(-2 * log(u1)) * cos(2 * pi * u2);
+        float u1 = rand01();
+        float u2 = rand01();
+        o[i] = sqrt((float)2 / n) * (float)sqrt(-2 * log(u1)) * cos(2 * pi * u2);
     }
 }
 
+/* m*n ビットで表される画像 x を、ランダムに 平行移動、拡大縮小、回転させ、新たな画像を生成し、それを o に保存する。 */
 void generate(int m, int n, const float *x, float *o) {
     float shift_i = rand01() * 4 - 2;
     float shift_j = rand01() * 4 - 2;
@@ -345,7 +307,7 @@ void momentum_update(int n, float *w, float eta, float alpha, float *dEdw,
 /* momentum SGD を一回分行う つまり
 1.訓練データをn個ずつ取り出し、それぞれに対しA1 ~ b3 の 勾配を計算し、平均をとる
 2.その勾配と前回の更新量によってA1~b3を更新する
-3. 1,2を訓練データの個数 / n 回繰り返す
+3. 1,2を　(訓練データの個数/n) 回繰り返す
 */
 
 void momentum_SGD(float *train_x, unsigned char *train_y, int train_count,
@@ -387,6 +349,8 @@ void momentum_SGD(float *train_x, unsigned char *train_y, int train_count,
             init(50, 0, dEdb1);
             init(100, 0, dEdb2);
             init(10, 0, dEdb3);
+            /*訓練データをそのまま使うのではなく、generate関数を用いて新しくデータを作成し、それによって
+            勾配を計算する*/
             float *train_x_new = malloc(sizeof(float) * 784);
             generate(28, 28, train_x + 784 * index[i * n + j], train_x_new);
             backward6(A1, b1, A2, b2, A3, b3, train_x_new,
@@ -411,7 +375,8 @@ void momentum_SGD(float *train_x, unsigned char *train_y, int train_count,
     }
 }
 
-void test(int epoch, float *A1, float *b1, float *A2, float *b2, float *A3,
+/* 各fc層のパラメータ、テストケースの個数、テストケース、答えを渡して、正答率及び損失関数の平均を計算*/
+void test(float *A1, float *b1, float *A2, float *b2, float *A3,
           float *b3, int test_count, float *test_x, unsigned char *test_y) {
     int correct = 0;
     float e = 0;
@@ -429,131 +394,6 @@ void test(int epoch, float *A1, float *b1, float *A2, float *b2, float *A3,
     
 }
 
-/* 配列で表された画像データを平行移動させる */
-void shift(int m, int n, const float *x, float *o, int shift_i, int shift_j) {
-    init(m * n, 0, o);
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-            if ((i - shift_i >= 0) && (i - shift_i < m) && (j - shift_j >= 0) &&
-                (j - shift_j < n)) {
-                o[i * n + j] = x[(i - shift_i) * n + (j - shift_j)];
-            }
-        }
-    }
-}
-
-/* 画像データを拡大・縮小する */
-void scaling(int m, int n, const float *x, float *o, float scale_i,
-             float scale_j) {
-    int ci = m / 2;
-    int cj = n / 2;
-
-    init(m * n, 0, o);
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-            int i_new = (i - ci) * scale_i + ci;
-            int j_new = (j - cj) * scale_j + cj;
-            if ((i_new >= 0) && (i_new < m) && (j_new >= 0) && (j_new < n)) {
-                o[i_new * n + j_new] = x[i * n + j];
-            }
-        }
-    }
-}
-
-/* 画像データの回転*/
-void rotation(int m, int n, const float *x, float *o, float theta) {
-    theta *= pi / 180;
-    int ci = m / 2;
-    int cj = n / 2;
-    init(m * n, 0, o);
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-            int i_new = (i - ci) * cos(theta) - (j - cj) * sin(theta) + ci;
-            int j_new = (i - ci) * sin(theta) + (j - cj) * cos(theta) + cj;
-            if ((i_new >= 0) && (i_new < m) && (j_new >= 0) && (j_new < n)) {
-                o[i_new * n + j_new] = x[i * n + j];
-            }
-        }
-    }
-}
-
-/* xのアダマール積を計算し、y に計算結果記録*/
-void Hadamard(int n, const float *x, float *y) {
-    for (int i = 0; i < n; i++) {
-        y[i] = x[i] * x[i];
-    }
-}
-
-void Adam_update(int n, float *v, float *h, float *w, float *dEdw, float eta) {
-    float beta1 = 0.9;
-    float beta2 = 0.999;
-    scale(n, beta1, v);
-    scale_and_add(n, (1 - beta1), dEdw, v);
-    scale(n, beta2, h);
-    Hadamard(n, dEdw, dEdw);
-    scale_and_add(n, (1 - beta2), dEdw, h);
-    float *dw = malloc(sizeof(float) * n);
-    sqrt_and_div(n, h, v, dw);
-    scale(n, -eta * sqrt(1 - beta2) / (1 - beta1), dw);
-    add(n, dw, w);
-    free(dw);
-}
-
-void Adam(float *train_x, unsigned char *train_y, int train_count, int n,
-          int *index, float *A1, float *b1, float *A2, float *b2, float *A3,
-          float *b3, float *v_A1, float *v_b1, float *v_A2, float *v_b2,
-          float *v_A3, float *v_b3, float eta, float *h_A1, float *h_b1,
-          float *h_A2, float *h_b2, float *h_A3, float *h_b3) {
-    for (int i = 0; i < train_count / n; i++) {
-        float *dEdA1_ave = malloc(sizeof(float) * 50 * 784);
-        float *dEdA2_ave = malloc(sizeof(float) * 50 * 100);
-        float *dEdA3_ave = malloc(sizeof(float) * 100 * 10);
-        float *dEdb1_ave = malloc(sizeof(float) * 50);
-        float *dEdb2_ave = malloc(sizeof(float) * 100);
-        float *dEdb3_ave = malloc(sizeof(float) * 10);
-        init(784 * 50, 0, dEdA1_ave);
-        init(50 * 100, 0, dEdA2_ave);
-        init(100 * 10, 0, dEdA3_ave);
-        init(50, 0, dEdb1_ave);
-        init(100, 0, dEdb2_ave);
-        init(10, 0, dEdb3_ave);
-        for (int j = 0; j < n; j++) {
-            float *dEdA1 = malloc(sizeof(float) * 50 * 784);
-            float *dEdA2 = malloc(sizeof(float) * 50 * 100);
-            float *dEdA3 = malloc(sizeof(float) * 100 * 10);
-            float *dEdb1 = malloc(sizeof(float) * 50);
-            float *dEdb2 = malloc(sizeof(float) * 100);
-            float *dEdb3 = malloc(sizeof(float) * 10);
-            init(50 * 784, 0, dEdA1);
-            init(100 * 50, 0, dEdA2);
-            init(100 * 10, 0, dEdA3);
-            init(50, 0, dEdb1);
-            init(100, 0, dEdb2);
-            init(10, 0, dEdb3);
-            float *train_x_new = malloc(sizeof(float) * 784);
-            generate(28, 28, train_x + 784 * index[i * n + j], train_x_new);
-            backward6(A1, b1, A2, b2, A3, b3, train_x_new,
-                      train_y[index[i * n + j]], dEdA1, dEdb1, dEdA2, dEdb2,
-                      dEdA3, dEdb3);
-            add_ave(50 * 784, n, dEdA1, dEdA1_ave);
-            add_ave(50 * 100, n, dEdA2, dEdA2_ave);
-            add_ave(100 * 10, n, dEdA3, dEdA3_ave);
-            add_ave(50, n, dEdb1, dEdb1_ave);
-            add_ave(100, n, dEdb2, dEdb2_ave);
-            add_ave(10, n, dEdb3, dEdb3_ave);
-            free_all(7, dEdA1, dEdA2, dEdA3, dEdb1, dEdb2, dEdb3, train_x_new);
-        }
-        Adam_update(50 * 784, v_A1, h_A1, A1, dEdA1_ave, eta);
-        Adam_update(50 * 100, v_A2, h_A2, A2, dEdA2_ave, eta);
-        Adam_update(10 * 100, v_A3, h_A3, A3, dEdA2_ave, eta);
-        Adam_update(50, v_b1, h_b1, b1, dEdb1_ave, eta);
-        Adam_update(100, v_b2, h_b2, b2, dEdb2_ave, eta);
-        Adam_update(10, v_b3, h_b3, b3, dEdb3_ave, eta);
-        free_all(6, dEdA1_ave, dEdA2_ave, dEdA3_ave, dEdb1_ave, dEdb2_ave,
-                 dEdb3_ave);
-    }
-}
-
 int main(int argc, char *argv[]) {
     init_genrand(time(NULL));
     srand(time(NULL));
@@ -568,30 +408,32 @@ int main(int argc, char *argv[]) {
     load_mnist(&train_x, &train_y, &train_count, &test_x, &test_y, &test_count,
                &width, &height);
 
-    int epoch = 10;
-    int n = 100;
-    float eta = 0.001;
-    float alpha = 0.9;
+    int epoch = 10; /*エポック数*/
+    int n = 100; /*ミニバッチサイズ*/
+    float eta = 0.1; /* 学習率*/
+    float alpha = 0.9; /* momentumの係数*/
+    /*Ai bi はfci層のパラメータ*/
     float *A1 = malloc(sizeof(float) * 784 * 50);
     float *A2 = malloc(sizeof(float) * 50 * 100);
     float *A3 = malloc(sizeof(float) * 100 * 10);
     float *b1 = malloc(sizeof(float) * 50);
     float *b2 = malloc(sizeof(float) * 100);
     float *b3 = malloc(sizeof(float) * 10);
-    load(argv[1], 50, 784, A1, b1);
+    /* load(argv[1], 50, 784, A1, b1);
     load(argv[2], 100, 50, A2, b2);
-    load(argv[3], 10, 100, A3, b3);
-    /* gaussian_rand_init(784 * 50, A1);
+    load(argv[3], 10, 100, A3, b3); */
+    gaussian_rand_init(784 * 50, A1);
     gaussian_rand_init(50 * 100, A2);
     gaussian_rand_init(100 * 10, A3);
     gaussian_rand_init(50, b1);
     gaussian_rand_init(100, b2);
-    gaussian_rand_init(10, b3); */
+    gaussian_rand_init(10, b3);
+
     int *index = malloc(sizeof(int) * train_count);
     for (int i = 0; i < train_count; i++) {
         index[i] = i;
     }
-    /* 変化量を表す変数*/
+    /* 各パラメータA1~b3について、前回の変化量を表す変数*/
     float *v_A1 = malloc(sizeof(float) * 50 * 784);
     float *v_A2 = malloc(sizeof(float) * 50 * 100);
     float *v_A3 = malloc(sizeof(float) * 100 * 10);
@@ -604,35 +446,20 @@ int main(int argc, char *argv[]) {
     init(50, 0, v_b1);
     init(100, 0, v_b2);
     init(10, 0, v_b3);
-    float *h_A1 = malloc(sizeof(float) * 50 * 784);
-    float *h_A2 = malloc(sizeof(float) * 50 * 100);
-    float *h_A3 = malloc(sizeof(float) * 100 * 10);
-    float *h_b1 = malloc(sizeof(float) * 50);
-    float *h_b2 = malloc(sizeof(float) * 100);
-    float *h_b3 = malloc(sizeof(float) * 10);
-    init(784 * 50, 0, h_A1);
-    init(50 * 100, 0, h_A2);
-    init(100 * 10, 0, h_A3);
-    init(50, 0, h_b1);
-    init(100, 0, h_b2);
-    init(10, 0, h_b3);
-    for (int i = 0; i < epoch; i++) {
-        shuffle(train_count, index);
-        momentum_SGD(train_x, train_y, train_count, n, index, A1, b1, A2, b2,
+    for (int i = 0; i < epoch; i++) { /*以下をepoch回繰り返す*/
+        shuffle(train_count, index); /*配列indexをシャッフル*/
+        momentum_SGD(train_x, train_y, train_count, n, index, A1, b1, A2, b2, /*momentum SGDの更新式によってA1~b3を更新*/
                      A3, b3, v_A1, v_b1, v_A2, v_b2, v_A3, v_b3, eta, alpha);
-        /* Adam(train_x, train_y, train_count, n, index, A1, b1, A2, b2, A3, b3,
-             v_A1, v_b1, v_A2, v_b2, v_A3, v_b3, eta, h_A1, h_b1, h_A2, h_b2,
-             h_A3, h_b3); */
-        printf("Epoch %3d/%d\n", i + 1, epoch);
-        printf("Train : ");
-        test(i + 1, A1, b1, A2, b2, A3, b3, train_count, train_x, train_y);
+        printf("Epoch %3d/%d\nTrain : ", i + 1, epoch); 
+        test(A1, b1, A2, b2, A3, b3, train_count, train_x, train_y); /*訓練データに対する正答率及び損失関数*/
         printf("Test : ");
-        test(i + 1, A1, b1, A2, b2, A3, b3, test_count, test_x, test_y);
+        test(A1, b1, A2, b2, A3, b3, test_count, test_x, test_y); /*テストデータに対する正答率及び損失関数*/
         printf("\n");
     }
-    free_all(6, v_A1, v_A2, v_A3, v_b1, v_b2, v_b3);
-    save("fc1.dat", 50, 784, A1, b1);
-    save("fc2.dat", 100, 50, A2, b2);
+    free_all(6, v_A1, v_A2, v_A3, v_b1, v_b2, v_b3); 
+    save("fc1.dat", 50, 784, A1, b1); /*各パラメータを保存*/ 
+    save("fc2.dat", 100, 50, A2, b2); 
     save("fc3.dat", 10, 100, A3, b3);
+    free_all(6, A1, b1, A2, b2, A3, b3);
     return 0;
 }
